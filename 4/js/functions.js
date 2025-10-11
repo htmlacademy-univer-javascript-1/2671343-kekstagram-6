a80e1f453335b21bb5a773b4f1da0eb7ab57a52c
Keks <keks@htmlacademy.ru>
2025-10-11 09:54:09 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/functions.js
@@ -0,0 +1,24 @@
+const checkLength = function (check, len) {return check.length <= len};
+
+const isPalindrome = function (check) {
+  check = check.replaceAll(' ', '').toLowerCase();
+  let reverseStr = '';
+  for (let i = check.length-1; i >= 0; i--) {
+    reverseStr += check[i];
+  }
+  return check === reverseStr;
+};
+
+const checkWorkMeetings = function (workStart, workEnd, meetStart, meetDur) {
+  const toMinutes = (timeStr) => {
+    const [h, m] = timeStr.split(':').map(Number);
+    return h * 60 + m;
+  };
+
+  const workStartMin = toMinutes(workStart);
+  const workEndMin = toMinutes(workEnd);
+  const meetStartMin = toMinutes(meetStart);
+  const meetEndMin = meetStartMin + meetDur;
+
+  return meetStartMin >= workStartMin && meetEndMin <= workEndMin;
+};
